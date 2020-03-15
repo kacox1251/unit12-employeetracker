5af56794b63961a98e4f525d3220c91eb6ae8f46
kacox1251 <56182547+kacox1251@users.noreply.github.com>
2020-03-15 15:44:10 -0600

    selector querys for mysql

--- db/schema.sql
+++ db/schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE department (
     id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR (30),
     PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE role (
     id INT AUTO_INCREMENT NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE role (
     salary DECIMAL (10, 2),
     department_id INT,
     PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE employee (
     id INT AUTO_INCREMENT NOT NULL,
@@ -24,5 +24,63 @@ CREATE TABLE employee (
     last_name VARCHAR (30),
     role_id INT,
     manager_id INT,
-    PRIMARY KEY id
-)
+    PRIMARY KEY (id)
+);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
+
+-- add employees, add departments, add roles
+-- update employee roles
+
+-- View all employees
+SELECT 
+    employee.id, 
+    employee.first_name, 
+    employee.last_name, 
+    department.name AS department, 
+    role.title, 
+    role.salary 
+FROM 
+    employee
+INNER JOIN 
+    role ON employee.role_id = role.id 
+INNER JOIN 
+    department ON role.department_id = department.id 
+ORDER BY 
+    employee.id ASC;
+
+-- View employees by department
+SELECT 
+	employee.id,
+    employee.first_name,
+    employee.last_name,
+    role.title,
+    department.name,
+    role.salary
+FROM
+    employee
+INNER JOIN
+    role ON employee.role_id = role.id
+INNER JOIN
+    department ON role.department_id = department.id
+WHERE
+	department.name = "Production";
+
+-- View employees by role
+SELECT 
+	employee.id,
+    employee.first_name,
+    employee.last_name,
+    role.title,
+    department.name,
+    role.salary
+FROM
+    employee
+INNER JOIN
+    role ON employee.role_id = role.id
+INNER JOIN
+    department ON role.department_id = department.id
+WHERE
+	role.title = "Coordinator";
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,24 +1,19 @@
 INSERT INTO department (name) VALUES 
 ("Sales"), 
 ("Production"),  
-("Logistics"), 
 ("Development");
 
 INSERT INTO role (title, salary,department_id) VALUES
 ("Accounts", 57000.00, 1),
-("Sales Manager", 60000.00, 1),
+("Manager", 60000.00, 1),
 ("Coordinator", 48000.00, 2),
-("Shipping and Receiving", 45000.00, 3),
-("Engineer", 60000.00, 4),
-("Customer Service Rep", 55000.00, 1);
+("Engineer", 60000.00, 4);
 
-INSERT INTO employee (first_name, last_name, role_id) VALUES
-("Selena", "Kyle", 1),
-("Bruce", "Wayne", 2),
-("Dick", "Greyson", 3),
-("Damian", "Wayne", 4),
-("Barbara", "Gordan", 5),
-("Alfred", "Pennyworth", 6);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
+("Selena", "Kyle", 1, NULL),
+("Bruce", "Wayne", 2, NULL),
+("Dick", "Greyson", 3, NULL),
+("Barbara", "Gordon", 4, NULL);
 
 SELECT * FROM department;
 SELECT * FROM role;
