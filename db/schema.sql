c08dbb8605b6909a8281ba22175ad47c5d2820d7
kacox1251 <56182547+kacox1251@users.noreply.github.com>
2020-03-15 14:09:49 -0600

    adding mysql db and tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,28 @@
--- department:
--- id - INT PRIMARY KEY
--- name - VARCHAR(30) to hold department name
+DROP DATABASE IF EXISTS employeeTrackerDB;
 
--- role:
--- id - INT PRIMARY KEY
--- title -  VARCHAR(30) to hold role title
--- salary -  DECIMAL to hold role salary
--- department_id -  INT to hold reference to department role belongs to
+CREATE DATABASE employeeTrackerDB;
 
--- employee:
--- id - INT PRIMARY KEY
--- first_name - VARCHAR(30) to hold employee first name
--- last_name - VARCHAR(30) to hold employee last name
--- role_id - INT to hold reference to role employee has
--- manager_id - INT to hold reference to another employee that manager of the current employee. This field may be null if the employee has no manager
+USE employeeTrackerDB;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR (30),
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR (30),
+    salary DECIMAL (10, 2),
+    department_id INT,
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR (30),
+    last_name VARCHAR (30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY id
+)
